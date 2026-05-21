35f8a5bf43c3c2688b87b7170ba088aa5d72b6a4
Asaph Isweka <iswekawenn@gmail.com>
2026-05-21 12:41:47 +0300

    Updates

---
+++ assets/solely-logo-DIbJK0qv.js
@@ -0,0 +1,1 @@
+const l="/Sole-ly-Marketplace-V2/assets/solely-logo-BUgSAjfC.svg";export{l};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Sole-ly-Marketplace-V2/sw.js', { scope: '/Sole-ly-Marketplace-V2/' })})}
